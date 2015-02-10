b14f5bff80a2465c296c5d73982ffd61b8307fa6
Jack Rains <jack@samueljrains.com>
2015-02-10 14:39:04 -0500

    JS component for process.php emailer script

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,43 @@
-// wait for the DOM to be loaded 
-$(document).ready(function() {  
-    $("#myForm").submit(function(){
-        if($.trim($('#message').val()) === ''){
-				alertify.error("At least say something!");			
-            return(false);
-        }
-        if($('#answer_value').val() != $('#answer_input').val()) {
-				alertify.error("Anti spam answers do not match.");			
-           return(false);
-        }
-    });    
-    $('#myForm').ajaxForm(function() { 
-        $('#myForm').trigger("reset"); 
-        alertify.success("Message sent.  Thank you!");
-    }); 
+//js to get formData, process form via AJAX, and return success/errors when necessary.
+$(document).ready(function() {
+    // process the form
+    $('form').submit(function(event) {
+        var formData = {
+            'name'        : $('input[name=name]').val(),
+            'email'       : $('input[name=email]').val(),         
+            'message'     : $('textarea[name=message]').val(),
+            'answerValue' : $('input[name=answerValue]').val(),           
+            'answerInput' : $('input[name=answerInput]').val()
+        };
+        // make AJAX call to PHP program that checks the input values, and anti-spam answers and returns corresponding JSON string. 
+        $.ajax({
+            type        : 'POST',
+            url         : 'process.php',
+            data        : formData,
+            dataType    : 'json',
+            encode      : true
+        })
+        .done(function(data) {
+            if (!data.success) {
+                if (data.errors.message) {
+                    alertify.error(data.errors.message);
+                }
+                if (data.errors.answerInput) {
+                    alertify.error(data.errors.answerInput);
+                }
+            } 
+            else {
+                //success so form is reset and user is notified via alert.
+                $('form').trigger("reset"); 
+                alertify.success("Message sent.  Thank you!");                  
+            }
+        })
+        .fail(function() {
+            alertify.error("Something went wrong.  Try again later.");
+            //TODO: call to php function errorLog to log error.
+            //console.log(data);
+        });
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
 }); 
